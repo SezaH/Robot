6a0989d7d30568b1dd097749d4e0342ec2aaec42
Seza Habibi <Seza Habibi>
2018-04-28 23:21:37 -0700

    add priority queue

---
+++ src/item_queue.ts
@@ -0,0 +1,22 @@
+import {
+    MinPriorityQueue,
+    MaxPriorityQueue,
+    QueueItem } from 'priority-queue-ts';
+
+class Item extends QueueItem {
+    public value = 1;
+    public label = 'one';
+
+}
+// let queue = new MinPriorityQueue();
+//
+// let item = new Item();
+//
+// queue.push(item);
+
+
+
+
+
+// console.log(queue.peek());
+console.log("hello world!");
