cd86c958ad2f4eedb78c9bf1a10edc113fa43ddf
atspeers <atspeers@ucdavis.edu>
2018-04-30 17:06:20 -0500

    added robot file i talked about

---
+++ src/robot.ts
@@ -0,0 +1,26 @@
+
+let SerialPort = require('serialport');
+
+
+class Robot {
+    private isConnected: boolean;
+    private port;
+    constructor() {
+        this.isConnected = false;
+    }
+    connect(portName: string) {
+       	this.port = new SerialPort(portName, {baudRate: 115200});
+        this.isConnected = true;
+    }
+    sendMessage(message: string){
+	this.port.write(message);
+    }
+    receiveMessage(){
+	console.log(this.port.read());
+    }
+}
+
+let robot = new Robot();
+robot.connect("/dev/ttyACM0");
+robot.sendMessage("@\r\n");
+robot.receiveMessage();
