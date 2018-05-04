c85cfdd5308ddc1f77457da757369beeec9deda7
blhough <benhuff322@gmail.com>
2018-05-04 11:26:38 -0700

    Format and clean up

--- src/item_queue.ts
+++ src/item_queue.ts
@@ -1,52 +1,41 @@
 export interface Item {
-   x: number;
-   y: number;
-   z: number;
-   encoder_value: number;
-   class_id: number;
-   class_name: string;
+  x: number;
+  y: number;
+  z: number;
+  encoderValue: number;
+  classID: number;
+  className: string;
 }
-const THRESHOLD = 100;// 10 mm off each side
+const THRESHOLD = 100; // 10 mm off each side
 
 
-class ItemQueue{
+export class ItemQueue {
 
-  items: Item[] = [];
+  private items: Item[] = [];
 
-  public insert(item:Item){
-      if (!this.isDuplicate(item.x, item.y, item.encoder_value)){
-        this.items.push(item);
-      }
+  public insert(item: Item) {
+    if (!this.isDuplicate(item.x, item.y, item.encoderValue)) {
+      this.items.push(item);
+    }
   }
-  private isDuplicate(x:number, y: number, encoder_value: number,){
-    for(const item of this.items){
-      if (Math.pow(item.x + (encoder_value - item.encoder_value)- x, 2)+ Math.pow((item.y - y),2) <  THRESHOLD ){
-         console.log("duplicate true");
-          return true;
+
+  public delete(index: number) {
+    this.items.splice(index, 1);
+  }
+
+  public display() {
+    for (const item of this.items) console.log(item.x);
+  }
+
+  private isDuplicate(x: number, y: number, encoderValue: number) {
+    for (const item of this.items) {
+      if (Math.pow(item.x + (encoderValue - item.encoderValue) - x, 2) + Math.pow((item.y - y), 2) < THRESHOLD) {
+        console.log('duplicate true');
+        return true;
       }
     }
     return false;
   }
-  public delete(index:number){
-     this.items.splice(index, 1);
-  }
-
-  public display(){
-    for(const item of this.items)
-       console.log(item.x);
-  }
-
 }
-const cup1: Item = {x: 10,  y : 10, z:10, encoder_value: 10, class_id: 1, class_name:"cup" };
-const cup2: Item = {x: 11,  y : 11, z:11, encoder_value: 11, class_id: 1, class_name:"cup" };
-const cup3: Item = {x: 20,  y : 20, z:20, encoder_value: 20, class_id: 1, class_name:"cup" };
 
-var obj = new ItemQueue();
-   obj.insert(cup1);
-   obj.insert(cup2);
-   obj.insert(cup3);
 
-   obj.display();
-   obj.delete(0);
-   console.log("hello");
-   obj.display();
--- src/renderer.ts
+++ src/renderer.ts
@@ -1,7 +1,24 @@
+import { Item, ItemQueue } from './item_queue';
 import { Util } from './utils';
 
 async function main() {
   // Code here runs on page load.
+
+  const cup1: Item = { x: 10, y: 10, z: 10, encoderValue: 10, classID: 1, className: 'cup' };
+  const cup2: Item = { x: 11, y: 11, z: 11, encoderValue: 11, classID: 1, className: 'cup' };
+  const cup3: Item = { x: 20, y: 20, z: 20, encoderValue: 20, classID: 1, className: 'cup' };
+
+  const obj = new ItemQueue();
+  obj.insert(cup1);
+  obj.insert(cup2);
+  obj.insert(cup3);
+
+  obj.display();
+  obj.delete(0);
+  console.log('hello');
+  obj.display();
+
+
 }
 
 // Binds a function to the 'click' event of the html element with 'browse-btn' id.
