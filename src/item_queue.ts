f7e617d28548ff16649c6b56543eb7b13fdfd88a
Seza Habibi <Seza Habibi>
2018-05-04 07:12:02 -0700

    first try to build data structure

--- src/item_queue.ts
+++ src/item_queue.ts
@@ -1,22 +1,32 @@
-import {
-    MinPriorityQueue,
-    MaxPriorityQueue,
-    QueueItem } from 'priority-queue-ts';
+export interface Item {
+   x: number;
+   y: number;
+   z: number;
+   encoder_value: number;
+   class_id: number;
+   class_name: string;
+}
+const THRESHOLD = 100;// 10 mm off each side
 
-class Item extends QueueItem {
-    public value = 1;
-    public label = 'one';
+
+class ItemQueue{
+  items: Item[] = [];
+
+  public insert(item:Item){
+      if (!this.isDuplicate(item.x, item.y, item.encoder_value)){
+        this.items.push(item);
+      }
+  }
+  private isDuplicate(x:number, y: number, encoder_value: number,){
+    for(const item of this.items){
+      if (Math.pow(item.x + (encoder_value - item.encoder_value)- x, 2)+ Math.pow((item.y - y),2) > THRESHOLD ){
+          return true;
+      }
+    }
+    return false;
+  }
+  public delete(index:number){
+     this.items.splice(index, 1);
+  }
 
 }
-// let queue = new MinPriorityQueue();
-//
-// let item = new Item();
-//
-// queue.push(item);
-
-
-
-
-
-// console.log(queue.peek());
-console.log("hello world!");
