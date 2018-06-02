4df06826492aaa19eacaf5481dbc58a89aa27cbd
thewitzbold <edenbernabe@gmail.com>
2018-06-01 20:57:37 -0700

    prevents from running multiple times in the background the cv

--- src/model.ts
+++ src/model.ts
@@ -2,10 +2,18 @@ import { spawn } from 'child_process';
 
 export class Model {
   private mol = spawn('echo', ['Hello Michael']);
+  private running = false;
 
   public Run(modelName: string, pbTXT: string, percentage: string) {
+    if (this.running === true) {
+      console.log('it is running in the background');
+      return;
+    }
+
+    
     // tslint:disable-next-line:max-line-length
     this.mol = spawn('python3', ['io_object_detection.py', modelName, pbTXT, percentage], { cwd: '../models/research/object_detection' });
+    this.running = true;
 
     console.log('running');
     this.mol.stdout.on('data', (data) => {
@@ -18,6 +26,7 @@ export class Model {
 
     this.mol.on('exit', (code) => {
       if (code !== 0) {
+        this.running = false;
         console.log('Failed: ' + code);
       }
     });
@@ -25,6 +34,7 @@ export class Model {
 
   public Stop() {
     console.log('kill! ');
+    this.running = false;
     this.mol.kill();
   }
 }
